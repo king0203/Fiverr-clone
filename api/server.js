784bdefe2a7508e05cfc5935794ad1efd46bcae2
king0203 <darshanarkhade@gmail.com>
2023-09-02 03:11:33 +0530

    Design mongo schemas

--- api/server.js
+++ api/server.js
@@ -1,22 +1,20 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
 
 const connect = async () => {
-    try {
-      await mongoose.connect(process.env.MONGO);
-      console.log("Connected to mongoDB!");
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB!");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-app.listen(8080, () =>{
-    connect();
-    console.log('Server is running on port 8080')
+app.listen(8080, () => {
+  connect();
+  console.log("Server is running on port 8080");
 });
-
